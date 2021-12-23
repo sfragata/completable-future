40a9d8b139ace3e4dc5aa1208590e38f4b49a78c
Silvio Fragata da Silva <sfragata@gmail.com>
2021-12-23 14:03:13 -0500

    upgrade

--- src/main/java/com/github/sfragata/completablefuture/Process.java
+++ src/main/java/com/github/sfragata/completablefuture/Process.java
@@ -1,40 +1,44 @@
 package com.github.sfragata.completablefuture;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.math.BigDecimal;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 public class Process {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Process.class);
     public Process() {
     }
 
     void process(
         final InputParameter inputParameter) {
 
-        System.out.printf("process::%s%n", printCurrentThread());
+        LOGGER.info("process::{}", printCurrentThread());
 
         final CompletableFuture<Result> res =
             CompletableFuture.supplyAsync(() -> processInputParameter(inputParameter)).exceptionally(exception -> {
                 final Result result = new Result();
                 result.setStatus(Result.Status.ERROR);
                 result.setMessage(exception.getMessage());
-                System.out.printf("exceptionally::%s - status::%s%n", printCurrentThread(), result);
+                LOGGER.info("exceptionally::{} - status::{}", printCurrentThread(), result);
 
                 return result;
             });
 
-        System.out.println(res);
+        LOGGER.info(String.valueOf(res));
 
     }
 
     Result processInputParameter(
         final InputParameter inputParameter) {
 
-        System.out.printf("processInputParameter::%s, input param: %s%n", printCurrentThread(), inputParameter);
+        LOGGER.info("processInputParameter::{}, input param: {}", printCurrentThread(), inputParameter);
         final Result result = new Result();
         try {
-            System.out.printf("processInputParameter::%s - sleeping %d ms.%n", printCurrentThread(),
+            LOGGER.info("processInputParameter::{} - sleeping {} ms.", printCurrentThread(),
                 TimeUnit.SECONDS.toMillis(inputParameter.getWaitTimeoutSecs()));
             Thread.sleep(TimeUnit.SECONDS.toMillis(inputParameter.getWaitTimeoutSecs()));
         } catch (final InterruptedException e) {
@@ -46,7 +50,7 @@ public class Process {
         }
 
         result.setStatus(Result.Status.SUCCESS);
-        System.out.printf("processInputParameter::%s - status::%s%n", printCurrentThread(), result);
+        LOGGER.info("processInputParameter::{} - status::{}", printCurrentThread(), result);
 
         return result;
     }
@@ -59,7 +63,7 @@ public class Process {
     public static void main(
         final String[] args) {
 
-        System.out.printf("main-start::%s%n", Thread.currentThread().getName());
+        LOGGER.info("main-start::{}", Thread.currentThread().getName());
 
         final InputParameter inputParameter1 = new InputParameter();
         inputParameter1.setAmount(BigDecimal.ONE);
@@ -74,16 +78,16 @@ public class Process {
         final Process process = new Process();
 
         process.process(inputParameter1);
-        System.out.printf("main-send::%s-%s%n", Thread.currentThread().getName(), inputParameter1);
+        LOGGER.info("main-send::{}-{}", Thread.currentThread().getName(), inputParameter1);
 
         process.process(inputParameter2);
-        System.out.printf("main-send::%s-%s%n", Thread.currentThread().getName(), inputParameter2);
+        LOGGER.info("main-send::{}-{}", Thread.currentThread().getName(), inputParameter2);
         try {
             Thread.sleep(TimeUnit.SECONDS.toMillis(10));
         } catch (final InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.printf("main-end::%s%n", Thread.currentThread().getName());
+        LOGGER.info("main-end::{}", Thread.currentThread().getName());
 
     }
 
